bb8a99fdd18c0b5f1d5965a72777c5d8da4aa007
jyoti patel <jyotipatelfyit77100@gmail.com>
2021-06-18 18:29:23 -0700

    added all files

--- src/componentes/Input.js
+++ src/componentes/Input.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-const Input = () =>{
+const Input = (text, setText) =>{
 
     const [text, setText] = useState('')
+    
+
+    const BinToDec = () =>{
+        let bin = parseInt(text, 2)
+        const dec = bin 
+    } 
 
     const onSubmit = e =>{
         e.preventDefault()  
  
-        setText('')      
+        setText('')    
+    }
+
+    Input.prototypes = {
+        value:PropTypes.number
     }
     return(
     <> 
-        <h2 className="result">
-            { !text? 'Decimal': text}
-        </h2>
+        
         <form onSubmit={onSubmit}>
             <input 
                 className='input-field' 
                 type="text" 
                 value={text} 
                 onChange={(e) => setText(e.target.value)} 
+                autoFocus
+
                 placeholder="Enter text here..." 
             />
         </form>
